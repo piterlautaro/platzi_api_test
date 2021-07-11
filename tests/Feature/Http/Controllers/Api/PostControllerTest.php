e2008a335a017f3342cb701f26e3cae8b7e7ad91
Pedro Jiménez <47641247+piterlautaro@users.noreply.github.com>
2021-07-11 19:22:31 -0400

    :zap: Add test show

--- tests/Feature/Http/Controllers/Api/PostControllerTest.php
+++ tests/Feature/Http/Controllers/Api/PostControllerTest.php
@@ -6,26 +6,28 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
+use App\Post;
+
 class PostControllerTest extends TestCase
 {
     use RefreshDatabase;
+
+    protected $test_data = ['title' => 'Testing post'];
 
     public function test_store()
     {
         // $this->withoutExceptionHandling();
 
-        $test_data = ['title' => 'Testing post'];
-
-        $response = $this->json('POST', '/api/posts', $test_data);
+        $response = $this->json('POST', '/api/posts', $this->test_data);
 
         $response->assertJsonStructure([
                         'id',
                         'title',
                         'created_at',
                         'updated_at'
-                    ])->assertJson($test_data)->assertStatus(201);
+                    ])->assertJson($this->test_data)->assertStatus(201);
 
-        $this->assertDatabaseHas('posts', $test_data);
+        $this->assertDatabaseHas('posts', $this->test_data);
     }
 
     public function test_validate_title()
@@ -38,5 +40,26 @@ class PostControllerTest extends TestCase
         //Estatus HTTP 422
         $response->assertStatus(422)
                     ->assertJsonValidationErrors('title');
+    }
+
+    public function test_show()
+    {
+        $post = factory(Post::class)->create();
+
+        $response = $this->json('GET', "/api/posts/$post->id");
+
+        $response->assertJsonStructure([
+            'id',
+            'title',
+            'created_at',
+            'updated_at'
+        ])->assertJson(['title' => $post->title])->assertStatus(200);
+    }
+
+    public function test_validate_show()
+    {
+        $response = $this->json('GET', '/api/posts/1000');
+
+        $response->assertStatus(404);
     }
 }
